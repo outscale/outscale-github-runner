86b4aa1c270c33b88f7ff6fabd5cb84174ddee09
Maxime Dufour <maxime.dufour@outscale.com>
2022-01-05 09:02:51 +0000

    Fix org runner

--- terraform/configuration.tf
+++ terraform/configuration.tf
@@ -1,10 +1,10 @@
 resource "shell_script" "configure_runner" {
   lifecycle_commands {
     create = <<-EOF
-        ANSIBLE_CONFIG=ansible.cfg ansible-playbook -e "token=${var.github_access_token}" -e "repo_url=${var.runner_repo_url}" -e "runner_scope=${var.runner_scope}" env_file/playbook.yml
+      ANSIBLE_CONFIG=ansible.cfg ansible-playbook -e "token=${var.github_access_token}" -e "repo_url=${var.runner_repo_url}" -e "runner_scope=${var.runner_scope}"  -e "org_name=$(echo ${var.runner_repo_url} | sed -r 's%^.*github\.com/([^/]+)/?.*%\1%')" env_file/playbook.yml
     EOF
     update = <<-EOF
-        ANSIBLE_CONFIG=ansible.cfg ansible-playbook -e "token=${var.github_access_token}" -e "repo_url=${var.runner_repo_url}" -e "runner_scope=${var.runner_scope}" env_file/playbook.yml
+      ANSIBLE_CONFIG=ansible.cfg ansible-playbook -e "token=${var.github_access_token}" -e "repo_url=${var.runner_repo_url}" -e "runner_scope=${var.runner_scope}"  -e "org_name=$(echo ${var.runner_repo_url} | sed -r 's%^.*github\.com/([^/]+)/?.*%\1%')" env_file/playbook.yml
     EOF
     read   = <<-EOF
         echo "{\"file\": \"$(cat env_file/playbook.yml|base64)\",
