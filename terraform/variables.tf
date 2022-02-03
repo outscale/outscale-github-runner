70f300253afe5e1293aefd25e7b3c42208c1672e
Maxime Dufour <maxime.dufour@outscale.com>
2022-02-03 15:40:01 +0000

    Set an image id  by default

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,6 @@ variable "access_key_id" {}
 variable "secret_key_id" {}
 variable "region" {}
 
-variable "image_id" {}
 variable "github_access_token" {}
 variable "runner_repo_url" {}
 variable "runner_scope" {}
