e6e32e36245b1352cc197ab127027ed7c0695c2a
Thiery Ouattara <opensource@outscale.com>
2023-12-12 13:41:41 +0000

    Enable setting labels and prefix on runner

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,8 @@
 variable "access_key_id" {}
 variable "secret_key_id" {}
-variable "region" {}
-variable image_id {}
 
 variable "github_access_token" {}
 variable "runner_repo_url" {}
 variable "runner_scope" {}
+variable "runner_labels" {}
+variable "runner_name_prefix" {}
